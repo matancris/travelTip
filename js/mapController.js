0b2056d3bf58c917ff0d1a5fe0b80908ffa41999
Matan Crispel <matan.cris@gmail.com>
2020-08-20 08:09:02 +0300

    style changes

--- js/mapController.js
+++ js/mapController.js
@@ -116,7 +116,7 @@ function closeModal() {
 
 
 document.querySelector('.btn').addEventListener('click', (ev) => {
-    mapService.panTo(35.6895, 139.6917);
+    panTo(35.6895, 139.6917);
 })
 
 function getPosition() {
